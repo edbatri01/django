58b8bcb17745181159d0ba1514d6740ca34f2bf9
EduDev011098 <183423@ids.upchiapas.edu.mx>
2020-02-11 23:08:18 -0600

    voy arriba

---
+++ Login/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, re_path
+from django.conf.urls import include
+from django.contrib.auth.models import User
+# from rest_framework import routers, serilizers, viewsets
+from Login.views import CustonAuthToken
+from Login import views
+
+urlpatterns = [
+    re_path(r'login/$', CustonAuthToken.as_view()),
+    re_path(r'example2_lista2/$', views.ExampleList2.as_view()),
+    #Hola soy eduardo
+]
+
+
